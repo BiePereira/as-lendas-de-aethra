792989ac4a3ab13b4d94514658302c3b0c0ef4df
Gabriel Pereira <mpereira.gabriel@gmail.com>
2026-09-06 17:35:46 -0300

    Adição de Workflow do Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://BiePereira.github.io",
+    base: "/as-lendas-de-aethra"
+});
