e13f300d76e5fa8a472ed1e650b172a9a3ac1f3f
Gabriel Pereira <mpereira.gabriel@gmail.com>
2026-09-09 23:25:20 -0300

    getPath e Esqueleto base da pg Kaelis

---
+++ src/data/personagens.js
@@ -0,0 +1,42 @@
+import imagemKaelis from "../assets/images/kaelis.png";
+import imagemKaelisHero from "../assets/images/kaelis-hero.png";
+import imagemGaren from "../assets/images/garen.jpg";
+import imagemRiel from "../assets/images/riel.png";
+
+export const kaelis = {
+    nome: "Kaelis",
+    descricao: "A druidesa de Becanlar.",
+    imagemCard: imagemKaelis.src,
+    imagemHero: imagemKaelisHero.src,
+    alt: "Kaelis partindo de Becanlar",
+    link: "personagens/kaelis",
+
+    frase: "Pelo menos eu tenho motivo para ter minhas orelhas assim. Pior é você com essas bolotinhas achatadas por aí. Tem certeza que sua mãe não te trouxe de Rook arrastado por um pé?",
+    raca: "Meio-elfa",
+    origem: "Becanlar",
+    descricao: "A menina sarcástica meio-elfa de Becanlar",
+    historia: "",
+    idade: "16 anos"
+}
+
+export const garen = {
+    nome: "Garen",
+    descricao: "O guardião de Vau dos Gansos.",
+    imagemCard: imagemGaren.src,
+    alt: "Garen chegando em Vardis",
+    link: "personagens/garen"
+}
+
+export const riel = {
+    nome: "Riel",
+    descricao: "O ladino de Vardis.",
+    imagemCard: imagemRiel.src,
+    alt: "Riel em uma taverna de Vênia",
+    link: "personagens/riel"
+}
+
+export const personagens = [
+    kaelis,
+    garen,
+    riel
+];
---
+++ src/utils/paths.js
@@ -0,0 +1,3 @@
+export function getPath(path) {
+    return `${import.meta.env.BASE_URL}${path}`;
+}
