dcc316d6e7a30ecef356be3c67e8e03e9073b73a
Gabriel Pereira <mpereira.gabriel@gmail.com>
2026-09-19 12:07:36 -0300

    Imagens .webp + images-hero

--- src/data/personagens.js
+++ src/data/personagens.js
@@ -1,14 +1,17 @@
 import imagemKaelis from "../assets/images/kaelis.png";
 import imagemKaelisHero from "../assets/images/kaelis-hero.png";
+
 import imagemGaren from "../assets/images/garen.jpg";
+
 import imagemRiel from "../assets/images/riel.png";
+import imagemRielHero from "../assets/images/riel-hero.png";
 
 export const kaelis = {
     nome: "Kaelis",
     slug: "kaelis",
     descricao: "A druidesa de Becanlar",
-    imagemCard: imagemKaelis.src,
-    imagemHero: imagemKaelisHero.src,
+    imagemCard: imagemKaelis,
+    imagemHero: imagemKaelisHero,
     alt: "Kaelis partindo de Becanlar",
     frase: "Pelo menos eu tenho motivo para ter minhas orelhas assim. Pior é você com essas bolotinhas achatadas por aí. Tem certeza que sua mãe não te trouxe de Rook arrastado por um pé?",
     raca: "Meio-elfa",
@@ -21,7 +24,7 @@ export const garen = {
     nome: "Garen",
     slug: "garen",
     descricao: "O guardião de Vau dos Gansos",
-    imagemCard: imagemGaren.src,
+    imagemCard: imagemGaren,
     alt: "Garen chegando em Vardis",
     frase: "Pouco me importa o ouro, que leve tudo. A informação que eu precisava ninguém irá me roubar.",
     idade: "19 anos"
@@ -31,7 +34,8 @@ export const riel = {
     nome: "Riel",
     slug: "riel",
     descricao: "O ladino de Vardis",
-    imagemCard: imagemRiel.src,
+    imagemCard: imagemRiel,
+    imagemHero: imagemRielHero,
     alt: "Riel em uma taverna de Vênia",
     frase: "Especialista não... Mestre.",
     idade: "11 anos"
