db4317d37fdf88d7d4b3916308272e100cc7214d
Gabriel Pereira <mpereira.gabriel@gmail.com>
2026-09-19 23:41:47 -0300

    Páginas Individuais de Personagens

--- src/data/personagens.js
+++ src/data/personagens.js
@@ -2,12 +2,14 @@ import imagemKaelis from "../assets/images/kaelis.png";
 import imagemKaelisHero from "../assets/images/kaelis-hero.png";
 
 import imagemGaren from "../assets/images/garen.jpg";
+import imagemGarenHero from "../assets/images/garen-hero.png";
 
 import imagemRiel from "../assets/images/riel.png";
 import imagemRielHero from "../assets/images/riel-hero.png";
 
 export const kaelis = {
     nome: "Kaelis",
+    nomeCompleto: "Kaelis Finch Cäel'onarirn",
     slug: "kaelis",
     descricao: "A druidesa de Becanlar",
     imagemCard: imagemKaelis,
@@ -16,29 +18,56 @@ export const kaelis = {
     frase: "Pelo menos eu tenho motivo para ter minhas orelhas assim. Pior é você com essas bolotinhas achatadas por aí. Tem certeza que sua mãe não te trouxe de Rook arrastado por um pé?",
     raca: "Meio-elfa",
     origem: "Becanlar",
-    historia: "",
-    idade: "16 anos"
+    historia: "Kaelis é uma jovem e habilidosa druidesa transmorfa mestiça criada sob o abrigo dos carvalhos ancestrais de Becanlar. Com o espírito livre e olhar atento para o desenho, a garota de sardas e cabelos cor de fogo luta para dominar sua essência mágica e provar seu valor diante de uma importante tarefa dada à ela por seu povo.",
+    idade: "16 anos",
+    trechoLivro: "Sem muitas considerações, agiu de maneira espontânea. Manteve o joelho esquerdo no chão e tocou com a palma da mão na terra úmida. Com a mão esquerda, socou com força seu cajado no solo, firmando-o com afinco. Estava em sua maior conexão com a natureza desde sempre, e sentia a dor dos espíritos antigos da floresta. Então respirou fundo, baixou o rosto, fechou os olhos e, ainda sem um Espírito Guardião que fosse seu patrono, clamou por aquele que era o mais antigo em alto tom de voz: “Mussgon!” Seus olhos verdes vibrantes arregalaram-se. Seus cabelos encharcados passaram a esvoaçar. A garota levantou-se e gritou, mas não era um grito humano, tampouco élfico. Era primitivo, animalesco, feroz.",
+    rumores: [
+        "Ouvi dizer que é na margem oeste de Vessethalanlör que mora a meia-elfa de cabelos de fogo. Dizem que ela carrega um caderno de desenhos e que os animais parecem parar para serem retratados por ela.",
+        "Ouvi de um mercador em Porto-Verde que a garota de Becanlar enfrentou ladrões e resgatou uma refém usando apenas seu cajado e uma magia de fogo.",
+        "Um guarda da paliçada jura que ela não tem nada de elfa urbana, mas é uma selvagem do oeste que se transforma em gata para andar pelos telhados."
+    ]
 }
 
 export const garen = {
     nome: "Garen",
+    nomeCompleto: "Garen Fallow",
     slug: "garen",
     descricao: "O guardião de Vau dos Gansos",
     imagemCard: imagemGaren,
+    imagemHero: imagemGarenHero,
     alt: "Garen chegando em Vardis",
     frase: "Pouco me importa o ouro, que leve tudo. A informação que eu precisava ninguém irá me roubar.",
-    idade: "19 anos"
+    origem: "Vau dos Gansos",
+    raca : "Humano",
+    historia: "Garen é um jovem caçador em treinamento para ser Protetor do Povoado em Vau dos Gansos. Movido por uma lealdade inabalável e por um espírito firme, o determinado jubano de cabelos trançados combina coragem incansável e armamentos gnômicos em uma jornada perigosa pela liberdade.",
+    idade: "19 anos",
+    trechoLivro: "Garen Fallow passaria facilmente despercebido na taverna Pimenta & Capincho, lotada com homens e anões, mesmo tendo acabado de pegar a recompensa com o comissário do povoado de Lomalto. Os cunhos encheram a algibeira, e garantiriam mais duas semanas de alimentos e pousadas em qualquer lugar, exceto ali. Garen não tinha a menor intenção de gastar mais um talho sequer naquele lugar. Havia terminado os preparativos para a viagem para o norte do Ducado de Vênia, ainda que a noite estivesse caindo. Verificou mais uma vez se a sua espada estava bem presa às costas. Estava. Atravessou o rifle gnômico no mesmo sentido da espada.",
+    rumores: [
+        "Dizem que um jovem caçador vem percorrendo as estradas de Juba carregando armas de fogo gnômicas e uma segue fazendo perguntas sobre uma jovem vidente.",
+        "Ouvi de um mercador em Lomalto que semana passada teve esse jovem que enfrentou um anão em uma luta de punhos limpos só para arrancar uma informação que precisava. Deve ser mentira.",
+        "Sim, o jovem esteve aqui mais cedo. Estava em uma discussão pesada, se recusava a abrir mão do seu cavalo por dinheiro algum. Disse que não tratava seus companheiros como mercadoria. Achei bonitinho."
+    ]
 }
 
 export const riel = {
     nome: "Riel",
+    nomeCompleto: "Riel Cinza",
     slug: "riel",
     descricao: "O ladino de Vardis",
     imagemCard: imagemRiel,
     imagemHero: imagemRielHero,
     alt: "Riel em uma taverna de Vênia",
     frase: "Especialista não... Mestre.",
-    idade: "11 anos"
+    origem: "Axxaya / Tanaren",
+    raca: "K'thari",
+    historia: "Riel é um perspicaz e sarcástico k'thari de pelagem cinza e garras afiadas. Engenhoso, esconde sob o deboche marcas profundas do passado, e usa de astúcia e perspicácia para descobrir pistas sobre seu objetivo mais pessoal.",
+    idade: "11 anos",
+    trechoLivro: "Ele sorria de volta, com o capuz recolhido às costas e a face completamente exposta. Era um k’thari de pelagem cinza escura, com tons mais claros na base dos bigodes e no maxilar, mais protuberantes na região do pescoço. Tinha marcas tigradas em cinza escuro, quase preto, e seu olhar fendido era cor de avelã com tons de verde. As pontas das orelhas possuíam pelugem levemente excedente, as quais vibravam em resposta às estridentes risadas do idoso.",
+    rumores: [
+         "Dizem que nas mesas clandestinas de Vardis vaga um k'thari vagabundo de pelagem cinza e olhar afiado, campeão imbatível no jogo de cumeada",
+        "Ouvi um guarda comentar que nem o Duque Serrano pode com ele, e que é um ser perigoso. Dizem que perambula pelas ruas e é capaz de sumir antes mesmo que você perceba que ele esteve ali.",
+        "Contam nos becos de Lorenburg que sua agilidade entre os telhados é incomparável e que suas garras são tão afiadas quanto a sua língua"
+    ]
 }
 
 export const personagens = [
